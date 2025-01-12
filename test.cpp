5cf60fbfe868af93187bfb762e83adf9acaa9fef
Arsalan Modirkhazeni <arsalan.modirkhazeni@mail.polimi.it>
2025-01-12 15:39:26 +0100

    Adding second scenario for the test

--- test.cpp
+++ test.cpp
@@ -6,16 +6,45 @@ struct BankAccount
     int balance = 0;
     BankAccount() {
     };
-    explicit  BankAccount(int balance) : balance(balance) {}        
+    explicit BankAccount(int balance) : balance(balance) {}
+    void deposit(int amount) {
+        balance += amount;
+    }
 };
 
+struct BankAccountTest : testing::Test
+{
+    /* data */
+    BankAccount *account;
 
-TEST(AccountTest, BankAccountStartsEmpty) {
-    BankAccount account;
-    EXPECT_EQ(0, account .balance);
+    BankAccountTest()
+    {
+        account = new BankAccount;
+    }
+
+    ~BankAccountTest()
+    {
+    }
+};
+
+TEST_F(BankAccountTest, BankAccountStartsEmpty)
+{
+    EXPECT_EQ(0, account->balance);
 }
 
-int main(int argc, char **argv) {
+TEST_F(BankAccountTest, CanDepositMoeny)
+{
+    account->deposit(100);
+    EXPECT_EQ(100, account->balance);
+}
+
+// TEST(AccountTest, BankAccountStartsEmpty) {
+//     BankAccount account;
+//     EXPECT_EQ(0, account .balance);
+// }
+
+int main(int argc, char **argv)
+{
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
